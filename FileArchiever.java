108b06551d0c35e958fa7f54a7505aef76b05663
Demid5 <demkozlov@yandex.ru>
2019-03-10 15:11:17 +0700

    second commit, try

--- FileArchiever.java
+++ FileArchiever.java
@@ -2,62 +2,42 @@ import java.io.*;
 
 public class FileArchiever {
 
-    private final static int BUFFER_SIZE = 512;
-    private Archiver archiver;
-
-    public FileArchiever()
+    public void compressData(FileInputStream istream, FileOutputStream ostream)
     {
-        archiver = new Archiver();
-    }
-
-
-    public void compressData(FileInputStream istream, FileOutputStream ostream)throws IOException
-    {
-        //init byte buffer
-        byte[] buffer = new byte[BUFFER_SIZE];
         //
+        try(
         BufferedInputStream bfs = new BufferedInputStream(istream);
-        BufferedOutputStream bws = new BufferedOutputStream(ostream);
-        //
-        String bufferRead,bufferWrite = "";
-        int lenBr,rightBoundary = 0,maxSizeCodePoint = 4;
-        int currentCodePoint,prevCodePoint = -1,rightCodePoint = 0;
-        String tail = "";
-        int countRead = -1;
-        //main loop
-        while((countRead = bfs.read(buffer)) != -1)
+        BufferedOutputStream bws = new BufferedOutputStream(ostream))
         {
-            bufferRead = new String(buffer,0,countRead);
-            //append prev tail
-            bufferRead = tail + bufferRead;
-            //work with bufferRead
-            lenBr = bufferRead.length() - maxSizeCodePoint;//сразу немного обрезаем(на всякий случай)
-            if(lenBr > 0) {
-                for (int offset = 0; offset < lenBr; ++offset) {
-                    currentCodePoint = bufferRead.codePointAt(offset);
-                    //work with codepoint
-                    if (currentCodePoint != prevCodePoint) {
-                        rightBoundary = offset;
-                        rightCodePoint = prevCodePoint;
+            //init
+            int currentByte = -1,prevByte = bfs.read();
+            int countOfByte = 1;
+            //
+            while((currentByte = bfs.read()) != -1)
+            {
+                if(currentByte == prevByte)
+                    countOfByte++;
+                else//write this
+                {
+                    bws.write(prevByte);
+                    while(countOfByte != 0) {
+                        bws.write(countOfByte % 10);
+                        countOfByte/=10;
                     }
-                    //update vars
-                    prevCodePoint = currentCodePoint;
-                    offset += Character.charCount(currentCodePoint);
+                    //update
+                    countOfByte = 1;
                 }
-                bufferWrite = bufferRead.substring(0, rightBoundary);
-                //at now archive this
-                bufferWrite = archiver.getCompressData(bufferWrite);
-                //specify tail as
-                tail = bufferRead.substring(rightBoundary,bufferRead.length());
-                bws.write(bufferWrite.getBytes());
+                prevByte = currentByte;
             }
-            else
-                tail = bufferRead;
-        }
-        //at now archive last tail
-        bufferWrite = archiver.getCompressData(tail);
-        bws.write(bufferWrite.getBytes());
-        bws.close();
-        bfs.close();
+            //at the end to write
+            bws.write(prevByte);
+            while(countOfByte != 0) {
+                bws.write(countOfByte % 10);
+                countOfByte/=10;
+            }
+        }catch (IOException e)
+        { ; }
+        //
+
     }
 }
--- FileRArchiever.java
+++ FileRArchiever.java
@@ -2,61 +2,29 @@ import java.io.*;
 
 public class FileRArchiever {
 
-    private final static int BUFFER_SIZE = 512;
-    private RArchieve rarchiver;
-
-    public FileRArchiever()
-    {
-        rarchiver = new RArchieve();
-    }
-
     public void decompressData(FileInputStream istream, FileOutputStream ostream)throws IOException
     {
-        //init byte buffer
-        byte[] buffer = new byte[BUFFER_SIZE];
-        //
-        BufferedInputStream bfs = new BufferedInputStream(istream);
-        BufferedOutputStream bws = new BufferedOutputStream(ostream);
-        //
-        String bufferRead,bufferWrite = "";
-        int lenBr,rightBoundary = 0,maxSizeCodePoint = 4;
-        int currentCodePoint,prevCodePoint = -1,rightCodePoint = 0;
-        String tail = "";
-        //
-        int countRead = -1;
-        //main loop
-        while((countRead = bfs.read(buffer)) != -1)
-        {
-            bufferRead = new String(buffer,0,countRead);
-            //work with bufferRead
-            lenBr = bufferRead.length() - maxSizeCodePoint;//сразу немного обрезаем(на всякий случай)
-            if(lenBr > 0) {
-                for (int offset = 0; offset < lenBr; ++offset) {
-                    currentCodePoint = bufferRead.codePointAt(offset);
-                    //work with codepoint
-                    if (currentCodePoint != prevCodePoint) {
-                        rightBoundary = offset;
-                        rightCodePoint = prevCodePoint;
-                    }
-                    //update vars
-                    prevCodePoint = currentCodePoint;
-                    offset += Character.charCount(currentCodePoint);
-                }
-                bufferWrite = bufferRead.substring(0, rightBoundary);
-                //at now archive this
-                bufferWrite = rarchiver.getDecompressData(bufferWrite);
-                //specify tail as
-                tail = bufferRead.substring(rightBoundary,bufferRead.length());
-                bws.write(bufferWrite.getBytes());
+        try(BufferedInputStream bfs = new BufferedInputStream(istream);
+            BufferedOutputStream bws = new BufferedOutputStream(ostream)) {
+            int symbol,number = 0;
+            long countSymbols = 0;
+            symbol = bfs.read();
+            while(true)
+            {
+                while((number = bfs.read()) != -1 && number <= 9 )
+                    countSymbols = countSymbols * 10 + number;
+                //write temp result
+                for(int i = 0;i < countSymbols;++i)
+                    bws.write(symbol);
+                //update
+                symbol = number;
+                countSymbols = 0;
+                //condition exit
+                if(number <= 9)
+                    break;
             }
-            else
-                tail = bufferRead;
-        }
-        //at now archive last tail
-        bufferWrite = rarchiver.getDecompressData(tail);
-        bws.write(bufferWrite.getBytes());
-        bws.close();
-        bfs.close();
+        }catch (IOException e)
+        { ; }
     }
 
 
